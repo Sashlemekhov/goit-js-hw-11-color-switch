f89e5bae6ecab5790eb2a9f6aa2b147ebea415c2
Sashlemekhov <67118539+Sashlemekhov@users.noreply.github.com>
2021-01-03 20:46:18 +0200

    add files with HW

---
+++ js/body-color-switch.js
@@ -0,0 +1,41 @@
+const btnStartRef = document.querySelector('[data-action="start"]');
+const btnStopRef = document.querySelector('[data-action="stop"]');
+let isActive; 
+let timer;
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+btnStartRef.addEventListener('click', changeColorHandler);
+btnStopRef.addEventListener('click', stopTimer);
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+function setBodyStyleHandler() {
+  document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
+};
+
+function changeColorHandler() {
+  if (isActive) {
+    return;
+  };
+
+  isActive = true;
+  setBodyStyleHandler();
+  timer = setInterval(setBodyStyleHandler, 1000);
+};
+
+function stopTimer() {
+  isActive = false;
+  clearInterval(timer);
+};
